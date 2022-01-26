7e6aa6b74f8065017a3eab6b8b20e2112c9e6e12
Matheus Serafim <matheus18serafim@gmail.com>
2022-01-26 13:41:57 -0300

    fix: fixing bugs home

--- code-by-girls/src/Pages/Home/index.tsx
+++ code-by-girls/src/Pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import logo from "../../Assets/logo.svg";
+import logoMobile from "../../Assets/logoMobile.svg";
 import imgHome from "../../Assets/undraw_Dev_focus_re_6iwt 1.svg";
 import { useHistory } from "react-router-dom";
 
@@ -13,26 +14,19 @@ export const Home = () => {
       alignItems="center"
       justifyContent="center"
       flexDir={["column"]}
+      overflow="hidden"
     >
       <Heading display={["none", "none", "block"]}>
         <Image src={logo} alt="code-by-girls" />
       </Heading>
 
-      <Heading
-        alignItems="center"
-        justifyContent="center"
-        display={["flex", "flex", "none"]}
-      >
-        code-
-        <br />
-        by
-        <br />
-        -girls
+      <Heading mb="20px" display={["block", "block", "none"]}>
+        <Image src={logoMobile} alt="code-by-girls" />
       </Heading>
 
       <Flex justifyContent={["center"]} alignItems={["center"]}>
         <Button
-          w={["100px", "100px", "100px", "150px"]}
+          w={["200px", "200px", "100px", "150px"]}
           color="gray.50"
           mr={["", "", "50px"]}
           bg="purple.500"
@@ -40,6 +34,9 @@ export const Home = () => {
             background: "purple.400",
           }}
           onClick={() => history.push("/login")}
+          position="relative"
+          left="65px"
+          top="10px"
         >
           Logar
         </Button>
@@ -50,7 +47,7 @@ export const Home = () => {
           w={["", "", "400px"]}
         />
         <Button
-          w={["100px", "100px", "100px", "150px"]}
+          w={["200px", "200px", "100px", "150px"]}
           color="gray.50"
           ml={["", "", "50px"]}
           bg="purple.500"
@@ -58,6 +55,9 @@ export const Home = () => {
             background: "purple.400",
           }}
           onClick={() => history.push("/register")}
+          position="relative"
+          right="65px"
+          top="60px"
         >
           Registrar
         </Button>

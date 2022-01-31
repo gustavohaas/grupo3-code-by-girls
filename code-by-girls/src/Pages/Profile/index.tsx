381ed028a5cedb01ee003c1c58b817154400f971
Gustavo B. Haas <gbhaas97@gmail.com>
2022-01-31 14:52:21 -0300

    update: updated branch with up to date develop

--- code-by-girls/src/Pages/Profile/index.tsx
+++ code-by-girls/src/Pages/Profile/index.tsx
@@ -5,6 +5,7 @@ import { useProfile } from "../../Providers/Profile";
 import { useLogin } from "../../Providers/Login";
 import { CardSkills } from "../../Components/CardSkills";
 import { CardWorks } from "../../Components/CardWorks";
+import Header from "../../Components/Header/header";
 
 export const Profile = () => {
 
@@ -16,99 +17,102 @@ export const Profile = () => {
   },[])
 
   return (
-    <Flex w="100vw" h="100vh" bg="gray.50" justifyContent="center">
-      <Flex flexDir="column" w="65%" marginY="5">
-        <Box h="100%" w="90%">
-          <Box
-            bg="purple.100"
-            display="flex"
-            h="100px"
-            flexDir="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Heading as="h3" fontSize="2xl" color="gray.50" p="5">
-              Minhas Habilidades
-            </Heading>
+    <>
+      <Header input profile />
+      <Flex w="100vw" h="100vh" bg="gray.50" justifyContent="center">
+        <Flex flexDir="column" w="65%" marginY="5">
+          <Box h="100%" w="90%">
             <Box
-              as="button"
-              bg="none"
-              _hover={{ color: "gray.200" }}
-              m="10"
-              fontSize="2xl"
-              color="white"
+              bg="purple.100"
+              display="flex"
+              h="100px"
+              flexDir="row"
+              justifyContent="space-between"
+              alignItems="center"
             >
-              <BsFillPlusSquareFill />
+              <Heading as="h3" fontSize="2xl" color="gray.50" p="5">
+                Minhas Habilidades
+              </Heading>
+              <Box
+                as="button"
+                bg="none"
+                _hover={{ color: "gray.200" }}
+                m="10"
+                fontSize="2xl"
+                color="white"
+              >
+                <BsFillPlusSquareFill />
+              </Box>
             </Box>
+            <Flex
+              bg="white"
+              border="2px solid"
+              borderColor="purple.400"
+              h="298px"
+            >
+              {skills?.map((skill) => <CardSkills skill={skill} />)}
+              
+            </Flex>
           </Box>
-          <Flex
-            bg="white"
-            border="2px solid"
-            borderColor="purple.400"
-            h="298px"
-          >
-            {skills?.map((skill) => <CardSkills skill={skill} />)}
-            
-          </Flex>
-        </Box>
 
-        <Box w="90%">
-          <Box
-            bg="purple.100"
-            display="flex"
-            h="100px"
-            flexDir="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Heading as="h3" fontSize="2xl" color="gray.50" p="5">
-              Meus trabalhos
-            </Heading>
+          <Box w="90%">
             <Box
-              as="button"
-              w="15"
-              _hover={{ color: "gray.200" }}
-              m="10"
-              fontSize="2xl"
-              color="white"
+              bg="purple.100"
+              display="flex"
+              h="100px"
+              flexDir="row"
+              justifyContent="space-between"
+              alignItems="center"
             >
-              <BsFillPlusSquareFill />
+              <Heading as="h3" fontSize="2xl" color="gray.50" p="5">
+                Meus trabalhos
+              </Heading>
+              <Box
+                as="button"
+                w="15"
+                _hover={{ color: "gray.200" }}
+                m="10"
+                fontSize="2xl"
+                color="white"
+              >
+                <BsFillPlusSquareFill />
+              </Box>
             </Box>
+            <Flex
+              bg="white"
+              border="2px solid"
+              borderColor="purple.400"
+              h="298px"
+            >
+              {works?.map((work) => <CardWorks work={work} />)}
+            </Flex>
           </Box>
-          <Flex
-            bg="white"
-            border="2px solid"
-            borderColor="purple.400"
-            h="298px"
-          >
-            {works?.map((work) => <CardWorks work={work} />)}
-          </Flex>
-        </Box>
-      </Flex>
-      <Flex w="20vw">
-        <Box m="5" h="100vh" w="90%">
-          <Box
-            bg="purple.100"
-            display="flex"
-            h="100px"
-            flexDir="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Heading as="h3" fontSize="2xl" color="gray.50" p="5">
-              Grupos inscritos
-            </Heading>
+        </Flex>
+        <Flex w="20vw">
+          <Box m="5" h="100vh" w="90%">
+            <Box
+              bg="purple.100"
+              display="flex"
+              h="100px"
+              flexDir="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Heading as="h3" fontSize="2xl" color="gray.50" p="5">
+                Grupos inscritos
+              </Heading>
+            </Box>
+            <Flex
+              h="84.1%"
+              bg="white"
+              border="2px solid"
+              borderColor="purple.400"
+            >
+              
+            </Flex>
           </Box>
-          <Flex
-            h="84.1%"
-            bg="white"
-            border="2px solid"
-            borderColor="purple.400"
-          >
-            
-          </Flex>
-        </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };

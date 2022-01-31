ed65c474d5018aad767c81965f9352762781e3bc
Miguel Leite <miguelleite360@gmail.com>
2022-01-31 11:01:11 -0300

    feature: created header component

---
+++ code-by-girls/src/Pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@chakra-ui/react";
+import Header from "../../Components/Header/header";
+const Dashboard = () => {
+  return (
+    <Grid>
+      <Header input profile />
+    </Grid>
+  );
+};
+
+export default Dashboard;

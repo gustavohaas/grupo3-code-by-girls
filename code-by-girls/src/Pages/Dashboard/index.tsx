61320d6095b45fefd5e0cb3aadd5f82fe25ec304
jonatas heiderich <jf.heiderich@gmail.com>
2022-01-31 22:48:36 -0300

    update: card groups renderization

--- code-by-girls/src/Pages/Dashboard/index.tsx
+++ code-by-girls/src/Pages/Dashboard/index.tsx
@@ -1,9 +1,30 @@
-import { Grid } from "@chakra-ui/react";
+import { Box, Flex, Grid } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { CardGroup } from "../../Components/CardGroups";
 import Header from "../../Components/Header/header";
+import { useDashboard } from "../../Providers/Dashboard";
+import { useLogin } from "../../Providers/Login";
+
 const Dashboard = () => {
+  const { groups, loadGroups } = useDashboard();
+  const { data } = useLogin();
+
+  useEffect(() => {
+    loadGroups(data.user.id, data.accessToken).catch((err) => console.log(err));
+  });
+
   return (
     <Grid>
       <Header input profile />
+      <Flex justifyContent="center" mt="8">
+        <Flex w="75%" flexDir="row" flexWrap="wrap">
+          {groups.map((item) => (
+            <Box key={item.id}>
+              <CardGroup group={item} />
+            </Box>
+          ))}
+        </Flex>
+      </Flex>
     </Grid>
   );
 };

fb89bd50f9702e3a9d961281405391bd75a72dd6
Matheus Serafim <matheus18serafim@gmail.com>
2022-02-02 18:01:53 -0300

    feat: creating skeleton of pageSearchGroups

--- code-by-girls/src/Pages/Dashboard/index.tsx
+++ code-by-girls/src/Pages/Dashboard/index.tsx
@@ -2,29 +2,37 @@ import { Box, Flex, Grid } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { CardGroup } from "../../Components/CardGroups";
 import Header from "../../Components/Header/header";
+import { SkeletonSearch } from "../../Components/Skeletons/SkeletonSearch";
 import { useDashboard } from "../../Providers/Dashboard";
 import { useLogin } from "../../Providers/Login";
 import { useProfile } from "../../Providers/Profile";
 
 const Dashboard = () => {
-  const { groups } = useDashboard();
+  const { groups, loadGroups, searchNotFound, notFound } = useDashboard();
   const { data } = useLogin();
   const { getUserData } = useProfile();
 
   useEffect(() => {
     getUserData(data.user.id, data.accessToken);
+    loadGroups(data.user.id, data.accessToken);
   }, []);
+
+  console.log(groups);
 
   return (
     <Grid>
       <Header input profile />
       <Flex justifyContent="center" mt="8">
         <Flex w="75%" flexDir="row" flexWrap="wrap">
-          {groups?.map((item) => (
-            <Box key={item.id}>
-              <CardGroup group={item} />
-            </Box>
-          ))}
+          {notFound ? (
+            <SkeletonSearch />
+          ) : (
+            groups?.map((item) => (
+              <Box key={item.id}>
+                <CardGroup group={item} />
+              </Box>
+            ))
+          )}
         </Flex>
       </Flex>
     </Grid>

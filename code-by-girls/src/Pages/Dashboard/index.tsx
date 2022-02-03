84d12f7040ec1307247064bc2faaa41a27827178
Matheus Serafim <matheus18serafim@gmail.com>
2022-02-02 21:11:46 -0300

    feat: creating skeleton of page groups

--- code-by-girls/src/Pages/Dashboard/index.tsx
+++ code-by-girls/src/Pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import { Box, Flex, Grid } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { CardGroup } from "../../Components/CardGroups";
 import Header from "../../Components/Header/header";
+import { SkeletonGroups } from "../../Components/Skeletons/SkeletonGroups";
 import { SkeletonSearch } from "../../Components/Skeletons/SkeletonSearch";
 import { useDashboard } from "../../Providers/Dashboard";
 import { useLogin } from "../../Providers/Login";
@@ -11,13 +12,14 @@ const Dashboard = () => {
   const { groups, loadGroups, searchNotFound, notFound } = useDashboard();
   const { data } = useLogin();
   const { getUserData } = useProfile();
+  const [loadingGroups, setLoadingGroups] = useState(true);
 
   useEffect(() => {
     getUserData(data.user.id, data.accessToken);
-    loadGroups(data.user.id, data.accessToken);
+    loadGroups(data.user.id, data.accessToken).then((_) =>
+      setLoadingGroups(false)
+    );
   }, []);
-
-  console.log(groups);
 
   return (
     <Grid>
@@ -26,6 +28,8 @@ const Dashboard = () => {
         <Flex w="75%" flexDir="row" flexWrap="wrap">
           {notFound ? (
             <SkeletonSearch />
+          ) : loadingGroups ? (
+            <SkeletonGroups repeatCount={6} m="10px" />
           ) : (
             groups?.map((item) => (
               <Box key={item.id}>
